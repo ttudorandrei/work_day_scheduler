702b874d5bd24186bc1ebe41fbe8820af11daf02
Tudor Tocan <tudor.tocan@gmail.com>
2021-04-01 12:15:28 +0100

    add boilerplate code for setting hourly events

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,9 +3,23 @@ const currentDate = () => {
   $("#currentDay").text(moment().format("dddd, MMMM Do"));
 };
 
+const setHourlyEvent = () => {
+  const emptyScheduleObject = JSON.parse(
+    localStorage.getItem("emptyScheduleObject")
+  );
+
+  if (emptyScheduleObject !== null) {
+    console.log("null");
+  } else {
+    console.log("exists");
+  }
+};
+
 const runOnReady = () => {
   //displays current date
   currentDate();
+
+  setHourlyEvent();
 };
 
 //runs the code on ready
